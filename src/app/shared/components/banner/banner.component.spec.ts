13e37d0cd5995dc22faeb902ef36a49a820248e3
Charleston Campos <charlinhocjc@gmail.com>
2024-02-16 11:48:39 -0300

    Desafio: Finalizar os testes dos componentes da pasta shared

--- src/app/shared/components/banner/banner.component.spec.ts
+++ src/app/shared/components/banner/banner.component.spec.ts
@@ -1,17 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Input } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BannerComponent } from './banner.component';
+
+@Component({
+  selector: 'app-banner',
+  template: ''
+})
+class MockBannerComponent {
+  @Input() title: string = '';
+}
 
 describe('BannerComponent', () => {
   let component: BannerComponent;
   let fixture: ComponentFixture<BannerComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [BannerComponent]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BannerComponent
+      ],
+      declarations: [ MockBannerComponent ]
     })
     .compileComponents();
-    
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(BannerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +33,13 @@ describe('BannerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve renderizar o título fornecido', () => {
+    const titulo = 'Teste de título';
+    component.title = titulo;
+    fixture.detectChanges();
+    const element = fixture.nativeElement.querySelector('h1');
+    expect(element.textContent).toContain(titulo);
   });
 });
